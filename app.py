a553cb6dc6f21ba35936c52407d8fca80502b788
tombye <tombaromba@gmail.com>
2014-07-10 21:55:35 +0100

    Make app get key from environment var

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
+key = os.environ.get('TENON_KEY')
 
 @app.route('/')
 def index():
     return 'App working'
+
+@app.route('/')
+def index():
+  if key != None:
+    return 'App working, key is set.'
+  else:
+    return 'App working, no key set.'
 
 if __name__ == '__main__':
     app.run()
