6de956488e8ecfc24e3d1bdcb8fde2c477d3f6e9
tombye <tombaromba@gmail.com>
2014-07-14 22:57:20 +0100

    Make proxy connect to Tenon

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, Response
 from functools import wraps
 import os
+import urllib
 
 app = Flask(__name__)
+tenon_api_url = 'http://beta.tenon.io/api/'
 key = os.environ.get('TENON_KEY')
 username_env = os.environ.get('USERNAME')
 password_env = os.environ.get('PASSWORD')
+
+def get_results(url):
+  params = urllib.urlencode({'url': url, 'key': key})
+  connection = urllib.urlopen(tenon_api_url, params)
+  return connection.read()
 
 def keys_set():
   return (key != None and username_env != None and password_env != None)
@@ -32,11 +39,12 @@ def requires_auth(f):
     return f(*args, **kwargs)
   return decorated
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 @requires_auth
 def index():
+  url = request.args.get('url')
   if keys_set():
-    return 'App working, keys are set.'
+    return get_results(url)
   else:
     return 'App working, keys are missing or incorrect'
 
